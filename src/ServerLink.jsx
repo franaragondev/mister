d833536fe43ef9cee0a543eda6d8aa08e670d5a7
Fran Aragón <franaragon@gransliving.com>
2023-07-24 20:17:26 +0200

    Change server link

--- src/ServerLink.jsx
+++ src/ServerLink.jsx
@@ -1,4 +1,4 @@
 import Axios from 'axios';
 
-export default Axios.defaults.baseURL = 'http://localhost:8000';
-// export default Axios.defaults.baseURL = 'https://mister-be.herokuapp.com';
+// export default Axios.defaults.baseURL = 'http://localhost:8000';
+export default Axios.defaults.baseURL = 'https://mister-be.herokuapp.com';
